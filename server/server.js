5d53739706ac4915045728eb5e8bbb454b8a4625
bmhuyquoc104 <56634994+bmhuyquoc104@users.noreply.github.com>
2022-05-03 16:28:30 +0700

    5. Finish build server for backend on port 8000

---
+++ server/server.js
@@ -0,0 +1,26 @@
+// Import libraries
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// Add library to app
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+
+const PORT = process.env.PORT || 3000;
+const HOSTNAME = process.env.HOSTNAME || 'localhost';
+const mongoUrl = process.env.MONGO_URL;
+mongoose.connect(mongoUrl);
+
+// Run the server
+app.listen(PORT,HOSTNAME,() => {
+    console.log(`Server is running at http://${HOSTNAME}:${PORT}`);
+});
+
+// Render the content
+app.get('/',(req,res) => {
+    res.status(200).send('This is backend')
+});
