e86ba1d376b6b7c9ac0fe3ab815bfef3ee0c8d81
Võ Quốc Huy <56634994+bmhuyquoc104@users.noreply.github.com>
2022-05-06 12:09:21 +0700

    19. Install redux toolkit, redux react libraries and set up folder for reduxToolkt

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,21 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
-
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 const queryClient = new QueryClient();
+
+const store = configureStore({
+  reducer: {},
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
-    <React.StrictMode>
-      <App />
-      <ReactQueryDevtools initialIsOpen = {false} position = "bottom-right" />
-    </React.StrictMode>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+      </React.StrictMode>
+    </Provider>
   </QueryClientProvider>
 );
