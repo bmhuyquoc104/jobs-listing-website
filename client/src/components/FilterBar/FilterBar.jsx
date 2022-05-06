4ab5be25854a39253e78cc4e0a88a7e55b1d19e2
Võ Quốc Huy <56634994+bmhuyquoc104@users.noreply.github.com>
2022-05-06 15:31:35 +0700

    21. Finish delete key filter in filter bar

--- client/src/components/FilterBar/FilterBar.jsx
+++ client/src/components/FilterBar/FilterBar.jsx
@@ -5,12 +5,9 @@ import { useSelector, useDispatch } from "react-redux";
 import { deleteJob, clearJob } from "../../features/job";
 const FilterBar = () => {
   const jobs = useSelector((state) => state.jobs);
-  const dispatch = useDispatch();
   console.log(jobs);
-
-  const handleDelete = () => {
-    console.log("Delete the chip");
-  };
+  const dispatch = useDispatch();
+ 
   return (
     <>
       {/* If there are any items in filter arr -> display , else -> display none */}
@@ -32,32 +29,31 @@ const FilterBar = () => {
           }}
         >
           {jobs.map((job, index) => (
-              <Chip
-                key={index}
-                label={job}
-                sx={{
-                  color: "#72a19e",
-                  backgroundColor: "#eff6f6",
-                  fontSize: "0.5rem",
-                  height: "20px",
-                  borderRadius: "5px",
-                  padding: "0",
-                }}
-                onDelete={() => dispatch(clearJob())}
-                deleteIcon={
-                  <ClearIcon
-                    sx={{
-                      backgroundColor: "#72a19e",
-                      margin: "0 !important",
-                      color: "white !important",
-                      ":hover": { backgroundColor: "#000000" },
-                    }}
-                  />
-                }
-                size="medium"
-              ></Chip>
+            <Chip
+              key={index}
+              label={job}
+              sx={{
+                color: "#72a19e",
+                backgroundColor: "#eff6f6",
+                fontSize: "0.5rem",
+                height: "20px",
+                borderRadius: "5px",
+                padding: "0",
+              }}
+              onDelete={() => dispatch(deleteJob(index))}
+              deleteIcon={
+                <ClearIcon
+                  sx={{
+                    backgroundColor: "#72a19e",
+                    margin: "0 !important",
+                    color: "white !important",
+                    ":hover": { backgroundColor: "#000000" },
+                  }}
+                />
+              }
+              size="medium"
+            ></Chip>
           ))}
-          
         </Stack>
       ) : (
         <Stack mb={4}></Stack>
--- client/src/features/job.js
+++ client/src/features/job.js
@@ -1,22 +1,25 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const jobsSlice = createSlice({
-    name: "jobs",
-    initialState : [],
-    reducers: {
-        addJob: (state,action) => {
-            return state.concat(action.payload).filter((v, i, a) => a.indexOf(v) === i)
-
-        },
-        deleteJob:(state,action) => {
-            return state.filter((element,index) => element[index] !== action.payload)
-        },
-        clearJob:(state,action) =>{
-            return [];
-        }
-    }
+  name: "jobs",
+  initialState: [],
+  reducers: {
+    addJob: (state, action) => {
+      return state
+        .concat(action.payload)
+        .filter((v, i, a) => a.indexOf(v) === i);
+    },
+    deleteJob: (state, action) => {
+      return state.filter((element, index) => {
+        return index !== action.payload;
+      });
+    },
+    clearJob: () => {
+      return [];
+    },
+  },
 });
 
-export const {addJob,deleteJob,clearJob} = jobsSlice.actions;
+export const { addJob, deleteJob, clearJob } = jobsSlice.actions;
 
 export default jobsSlice.reducer;
